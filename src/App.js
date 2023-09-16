2d2f92af72febc4887f363ef43c71a3628d5ff92
David Delgado <david.delgadop88@gmail.com>
2023-09-16 18:17:01 -0400

    useEffect with no dependencies

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // useEffect runs during the first component evaluation and
+  // in every component re-evaluation ONLY if dependecies change.
+  useEffect(() => {
+    // None of this code is to be ran directly in the component function, if we did we could end up creating an infinite loop.
+    const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');  // Variable will be used to check if the user logged in.
+
+    if (storedUserLoggedInInformation === '1') {
+      setIsLoggedIn(true);
+    }
+
+  }, []); // if we don't list anything on the dependecies, then this useEffect will only run ONCE.
+
+
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+
+    // Storing in local storage, first argument is an identifier.
+    // second argument is the data we are storing, we are using it to indicate that we are logged in.
+    localStorage.setItem('isLoggedIn', '1');
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn'); // Removing the logged in info from local storage
+    setIsLoggedIn(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </React.Fragment>
   );
 }
 
